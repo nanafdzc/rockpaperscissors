80bf0afab3774934e01617d58f3a182b9c8646b4
Ana <anafc1998@gmail.com>
2019-08-23 19:47:04 -0300

    Added results and computer selection

--- script.js
+++ script.js
@@ -12,28 +12,59 @@ function computerPlay() {
     }
  } 
 
+ const displayResult = document.querySelector('#results');
+
  function playRound(playerSelection, computerSelection) {
-    
+  
+
     if (playerSelection == "paper" && computerSelection == "rock") {
-        return("You win! paper beats rock");
+        displayResult.textContent = "You win! paper beats rock";
 
     } else if (playerSelection == "rock" && computerSelection == "scissors") {
-        return "You win! rock beats scissors";
+        displayResult.textContent = "You win! rock beats scissors";
 
     } else if (playerSelection == "scissors" && computerSelection == "paper") {
-        return "You win! scissors beats paper";
+        displayResult.textContent = "You win! scissors beats paper";
 
     } else if (playerSelection == computerSelection) { 
-        return "It's a tie!";
+        displayResult.textContent = "It's a tie!";
 
     } else {
-        return "You lose! " + computerSelection + " beats " + playerSelection;
-    }
+        displayResult.textContent = "You lose! " + computerSelection + " beats " + playerSelection;
+    };
+};
 
-}
+function score(playerSelection, computerSelection) {
+            
+    if (playerSelection == "paper" && computerSelection == "rock") {
+         playerScore++;
+       
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        playerScore++;
+
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        playerScore++;
+
+    } else if (playerSelection == computerSelection) { 
+        
+
+    } else {
+        computerScore++;
+    }         
+};
 
 let computerSelection 
 let buttons = document.querySelectorAll('input');
+let playerScore = 0;
+let computerScore = 0;
+
+const selections = document.querySelector('#selections');
+const showSelection = document.createElement('div');
+showSelection.classList.add('showSelection');
+
+const gameScore = document.createElement('div');
+gameScore.classList.add('score');
+
 
 buttons.forEach((input) => {
 
@@ -41,6 +72,13 @@ input.addEventListener('click', (e) => {
    playerSelection = input.id;
    computerSelection = computerPlay();
    console.log(playRound(playerSelection,computerSelection));
+   console.log(score(playerSelection, computerSelection));
+
+   showSelection.textContent = 'Computer Selection: ' + computerSelection;
+   selections.appendChild(showSelection);
+
+   gameScore.textContent = ' Player : ' + playerScore + ' Computer : ' + computerScore;
+   selections.appendChild(gameScore);
     });
 });
 
